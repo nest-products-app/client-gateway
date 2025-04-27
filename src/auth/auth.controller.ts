2134c879beb4fbb6a218d9ed132a4aaa6faa621c
David-PX <david0nike@gmail.com>
2025-04-27 18:52:51 -0400

    Integrating auth-ms

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Get, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from 'src/interfaces/current-user.interface';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.client
+      .send({ cmd: 'auth.register.user' }, registerUserDto)
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send({ cmd: 'auth.login.user' }, loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return { user, token };
+  }
+}
